1b820dd7aabb784152ef647e047de50f5cbeb1f7
ShevchenyaIlya <shevchenya.i@gmail.com>
2021-03-05 16:29:38 +0300

    Add endpoint for registration and move role validation hear from db method

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -4,6 +4,7 @@ from flask import Blueprint, jsonify, request
 from flask_jwt_extended import create_access_token
 
 from backend.database_handler_entity import mongo
+from backend.role import role_validation
 
 auth: Blueprint = Blueprint('auth', __name__)
 
@@ -23,3 +24,26 @@ def login() -> Tuple[Any, int]:
 
     access_token = create_access_token(identity=str(user["_id"]))
     return jsonify(token=access_token, username=nickname, id=str(user["_id"])), 200
+
+
+@auth.route('/register', methods=["POST"])
+def register() -> Tuple[Any, int]:
+    body = request.get_json()
+
+    if (
+        not body
+        or not body.get("company", False)
+        or not body.get("username", False)
+        or not body.get("user_role", False)
+    ):
+        return jsonify(), 400
+
+    if not role_validation(body["user_role"]):
+        return jsonify(), 404
+
+    user_id = mongo.create_user(body["username"], body["user_role"], body["company"])
+
+    if not user_id:
+        return jsonify({"message": "User exist or achieved company members limit"}), 403
+
+    return jsonify(str(user_id)), 204
