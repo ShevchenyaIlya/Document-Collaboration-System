99829bdc3d51907167ac812b2c83813da0d103cc
ShevchenyaIlya <shevchenya.i@gmail.com>
2021-04-07 09:49:22 +0300

    Fix configurations

--- backend/config.py
+++ backend/config.py
@@ -5,13 +5,17 @@ from typing import cast
 
 class Config:
     def __init__(self) -> None:
-        self.JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-        self.MAIL_SERVER = os.getenv("MAIL_SERVER")
-        self.MAIL_PORT = int(cast(str, os.getenv("MAIL_PORT")))
-        self.MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-        self.MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-        self.MAIL_USE_TLS = strtobool(cast(str, os.getenv("MAIL_USE_TLS")))
-        self.MAIL_USE_SSL = strtobool(cast(str, os.getenv("MAIL_USE_SSL")))
+        self.JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "super-secret-key")
+        self.MAIL_SERVER = os.environ.get("MAIL_SERVER", "smtp.gmail.com")
+        self.MAIL_PORT = int(cast(str, os.environ.get("MAIL_PORT", 465)))
+        self.MAIL_USERNAME = os.environ.get(
+            "MAIL_USERNAME", "onlineproductstore95@gmail.com"
+        )
+        self.MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD", "OnlineProductStore")
+        self.MAIL_USE_TLS = strtobool(
+            cast(str, os.environ.get("MAIL_USE_TLS", "False"))
+        )
+        self.MAIL_USE_SSL = strtobool(cast(str, os.environ.get("MAIL_USE_SSL", "True")))
 
 
 CONFIG = Config()
